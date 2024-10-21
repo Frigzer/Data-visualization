31d5e4b179cc5dea7f4f89bd79440ce33817b6f9
Frigzer <borekadam89@gmail.com>
2024-10-21 12:10:58 +0200

    Dodaj pliki projektów.

---
+++ main.cpp
@@ -0,0 +1,160 @@
+﻿// Nagłówki
+//#include "stdafx.h"
+#include <GL/glew.h>
+#include <SFML/Window.hpp>
+
+#include <iostream>
+#include <cmath>
+
+// Kody shaderów
+const GLchar* vertexSource = R"glsl(
+#version 150 core
+in vec3 position;
+in vec3 color;
+out vec3 Color;
+void main(){
+	Color = color;
+	gl_Position = vec4(position, 1.0);
+}
+)glsl";
+
+const GLchar* fragmentSource = R"glsl(
+#version 150 core
+in vec3 Color;
+out vec4 outColor;
+void main()
+{
+	outColor = vec4(Color, 1.0);
+}
+)glsl";
+
+// Funkcja do sprawdzania błędów kompilacji shaderów
+void checkShaderCompilation(GLuint shader, const char* shaderName) {
+	GLint success;
+	GLchar infoLog[512];
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(shader, 512, NULL, infoLog);
+		std::cout << "Compilation " << shaderName << " ERROR\n" << infoLog << std::endl;
+	}
+	else {
+		std::cout << "Compilation " << shaderName << " OK" << std::endl;
+	}
+}
+
+// Funkcja do sprawdzania błędów linkowania programu
+void checkProgramLinking(GLuint program) {
+	GLint success;
+	GLchar infoLog[512];
+	glGetProgramiv(program, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(program, 512, NULL, infoLog);
+		std::cout << "Błąd linkowania programu: " << infoLog << std::endl;
+	}
+}
+
+int main()
+{
+	sf::ContextSettings settings;
+	settings.depthBits = 24;
+	settings.stencilBits = 8;
+
+	// Okno renderingu
+	sf::Window window(sf::VideoMode(800, 600, 32), "OpenGL", sf::Style::Titlebar | sf::Style::Close, settings);
+
+	// Inicjalizacja GLEW
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	// Utworzenie VAO (Vertex Array Object)
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	// Utworzenie VBO (Vertex Buffer Object)
+	// i skopiowanie do niego danych wierzchołkowych
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+
+	// Parametry wielokąta
+	const int numVertices = 6;
+	GLfloat vertices[6 * 6];
+	float radius = 0.5f;
+
+	// Generowanie współrzędnych w cylindrycznych
+	for (int i = 0; i < numVertices; ++i) {
+		float angle = 4.0f * 3.14 * i / numVertices;
+		vertices[i * 6] = radius * cos(angle);  // X
+		vertices[i * 6 + 1] = radius * sin(angle);  // Y
+		vertices[i * 6 + 2] = 0.0f;  // Z
+		// Kolory RGB
+		vertices[i * 6 + 3] = (i % 3 == 0) ? 1.0f : 0.0f;  // R
+		vertices[i * 6 + 4] = (i % 3 == 1) ? 1.0f : 0.0f;  // G
+		vertices[i * 6 + 5] = (i % 3 == 2) ? 1.0f : 0.0f;  // B
+	}
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// Utworzenie i skompilowanie shadera wierzchołków
+	GLuint vertexShader =
+		glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexSource, NULL);
+	glCompileShader(vertexShader);
+
+	// Utworzenie i skompilowanie shadera fragmentów
+	GLuint fragmentShader =
+		glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+	glCompileShader(fragmentShader);
+
+	// Sprawdzenie poprawności skompilowania shaderów
+	checkShaderCompilation(vertexShader, "vertexShader");
+	checkShaderCompilation(fragmentShader, "fragmentShader");
+
+	// Zlinkowanie obu shaderów w jeden wspólny program
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glBindFragDataLocation(shaderProgram, 0, "outColor");
+	glLinkProgram(shaderProgram);
+	glUseProgram(shaderProgram);
+
+	// Specifikacja formatu danych wierzchołkowych
+	GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+	glEnableVertexAttribArray(posAttrib);
+	glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), 0);
+	GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
+	glEnableVertexAttribArray(colAttrib);
+	glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+
+	// Rozpoczęcie pętli zdarzeń
+	bool running = true;
+	while (running) {
+		sf::Event windowEvent;
+		while (window.pollEvent(windowEvent)) {
+			switch (windowEvent.type) {
+			case sf::Event::Closed:
+				running = false;
+				break;
+			}
+		}
+		// Nadanie scenie koloru czarnego
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Narysowanie trójkąta na podstawie 3 wierzchołków
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// Wymiana buforów tylni/przedni
+		window.display();
+	}
+	// Kasowanie programu i czyszczenie buforów
+	glDeleteProgram(shaderProgram);
+	glDeleteShader(fragmentShader);
+	glDeleteShader(vertexShader);
+	glDeleteBuffers(1, &vbo);
+	glDeleteVertexArrays(1, &vao);
+	// Zamknięcie okna renderingu
+	window.close();
+	return 0;
+}
